6b030089891ae8e0cccb7164a15489961ce95cd1
BorisKarlof <boriskarlof122@gmail.com>
2022-10-16 18:13:18 +0300

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', loadChildren:() => import('./modules/auth/auth.module').then(m => m.AuthModule)},
+  {path:'main', loadChildren: ()=> import('./modules/main/main.module').then(m=>m.MainModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/main/main-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {MainComponent} from "./components/main/main.component";
+import {SettingsComponent} from "./components/settings/settings.component";
+
+
+const routes: Routes = [
+  {path:'', component: MainComponent, pathMatch:'full'},
+  {path:'settings', component: SettingsComponent, pathMatch: "full"}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class MainRoutingModule { }
